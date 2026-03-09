661b74147cfaed48040c00e70f133fbffcfd870b
Khoirul Anwar <shedenk@gmail.com>
2026-03-09 21:03:24 +0700

    modified:   index.html 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.tsx 	modified:   src/main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import Index from "./pages/Index";
 import Terms from "./pages/Terms";
 import SLA from "./pages/SLA";
@@ -19,6 +20,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      <Analytics />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
@@ -36,3 +38,4 @@ const App = () => (
 );
 
 export default App;
+
